4f782dc346efc2b3281181fe80f48b0e2da46f9e
Ruben-online <78996839+Ruben-online@users.noreply.github.com>
2024-09-30 21:40:16 -0600

    Archivo main, aqui hice todas las pruebas correspondientes para ver si funcionaba correctamente mi programa, psoteriormente le agregaré la interfaz gráfica.

---
+++ main.py
@@ -0,0 +1,18 @@
+from gif_manager import GIFManager
+
+
+def main():
+    directory = input("Ingrese la ruta de la carpeta con archivos GIF: ")
+
+    gif_manager = GIFManager(directory)
+    gif_manager.search_gif_files()
+
+    if gif_manager.gif_files:
+        print(f"Se encontraron {len(gif_manager.gif_files)} archivos GIF.")
+        gif_manager.process_gif_files()
+    else:
+        print("No se encontraron archivos GIF.")
+
+
+if __name__ == "__main__":
+    main()
