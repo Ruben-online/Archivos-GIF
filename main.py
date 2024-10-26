a6e9fffe53ce17dc0fd47cd540e4533d5439b9e5
Fernando Espinoza <rfespinoza01@gmail.com>
2024-10-26 16:53:55 -0600

    Cambios estéticos entorno a la implementación de poder guardar en .txt

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ from gif_manager import GIFManager
 
 def main():
     directory = input("Ingrese la ruta de la carpeta con archivos GIF: ")
+    output_file = "GIF's.txt"
 
     gif_manager = GIFManager(directory)
     gif_manager.search_gif_files()
 
     if gif_manager.gif_files:
-        print(f"\nSe encontró {len(gif_manager.gif_files)} archivos GIF")
-        gif_manager.process_gif_files()
+        print(f"Se encontraron {len(gif_manager.gif_files)} archivos GIF.")
+        gif_manager.process_gif_files(output_file)
+        print(f"Información de los archivos GIF guardada en {output_file}.")
     else:
-        print("No se encontró nigun archivo .gif")
+        print("No se encontraron archivos GIF.")
 
 
 if __name__ == "__main__":
